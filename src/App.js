43a762086e05f0fe14502ba2edf9939a98ff5a79
labarruffe <lucas_barruffe@hotmail.com>
2019-07-07 22:50:41 -0300

    Develop FastfoodForm

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+import FastfoodForm from './components/fastfood/FastfoodForm'
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<FastfoodForm />
+			</div>
+		)
+	}
+}
+
+export default App
---
+++ src/components/fastfood/FastfoodForm.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class FastfoodForm extends Component {
+	baseUrl = 'http://localhost:4200';
+
+	fastFoodArray = [];
+	
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			id: '',
+			name: '',
+            ingredients: '',
+            price: 0
+        }
+	}
+
+	changeHandler = e => {
+		this.setState({ [e.target.name]: e.target.value })
+	}
+	
+	submitHandler = e => {
+		e.preventDefault();
+		console.log(this.state);
+		axios
+			.post(`${this.baseUrl}/fastfoods`, this.state)
+			.then(response => {
+				console.log(response);
+				this.fastFoodArray.push(response);
+			})
+			.catch(error => {
+				console.log(error);
+			})
+	}
+
+	render() {
+		const { name, ingredients, price } = this.state;
+		return (
+			<form onSubmit={this.submitHandler}>
+				<label>
+				Nome
+				<br />
+				<input
+					name="name"
+					type="text"
+					value={name}
+					onChange={this.changeHandler}
+				/>
+				</label>
+				<br />
+				<br />
+				<label>
+				Ingredientes
+				<br />
+				<input
+					name="ingredients"
+					type="text"
+					value={ingredients}
+					onChange={this.changeHandler}
+				/>
+				</label>
+				<br />		
+				<br />
+				<label>
+				Valor
+				<br />
+				<input
+					name="price"
+					type="number"
+					value={price}
+					step="any"
+					onChange={this.changeHandler}
+				/>
+				</label>
+				<br />
+				<br />
+				<button type="submit">Salvar</button>
+			</form>
+		)
+	}
+}
+
+export default FastfoodForm
