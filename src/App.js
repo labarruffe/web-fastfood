ae287fc7fa8bc8ef3ae0e82a4706866fcb327283
labarruffe <lucas_barruffe@hotmail.com>
2019-07-09 19:02:15 -0300

    Add grid layout with card, row and col

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react'
 import './App.css'
 import FastfoodForm from './components/fastfood/FastfoodForm'
 import FastfoodList from './components/fastfood/FastfoodList'
+import { Card, Row, Col } from 'reactstrap';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<FastfoodForm />
-				<FastfoodList />
+			<div className="App container">
+				<div>
+					<Row>
+						<Col sm="2">
+						</Col>
+						<Col sm="8">
+							<Card body>
+								<FastfoodForm />
+								<FastfoodList />
+							</Card>
+						</Col>
+						<Col sm="2">
+						</Col>
+					</Row>
+				</div>
 			</div>
 		)
 	}
