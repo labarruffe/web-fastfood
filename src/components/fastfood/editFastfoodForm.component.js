3baa7fa65f1f2e7a4d5ab6fd8bb4695cdbeae252
labarruffe <lucas_barruffe@hotmail.com>
2019-07-14 22:01:22 -0300

    Create editfastfood component

---
+++ src/components/fastfood/editFastfoodForm.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditFastfoodForm extends Component {
+    render() {
+        return (
+            <div>
+                <p>EditFastfoodForm</p>
+            </div>
+        );
+    }
+}
