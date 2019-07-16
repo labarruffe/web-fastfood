af41b84fe2abd914814f47b01ce0803808bb8be3
labarruffe <lucas_barruffe@hotmail.com>
2019-07-15 23:16:56 -0300

    Improve create fastfood component

--- src/components/fastfood/createFastfoodForm.component.js
+++ src/components/fastfood/createFastfoodForm.component.js
@@ -1,83 +1,103 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
-class FastfoodForm extends Component {
+
+export default class FastfoodForm extends Component {
 	baseUrl = 'http://localhost:4200';
 
 	constructor(props) {
 		super(props);
 
+		this.onChangeName = this.onChangeName.bind(this);
+		this.onChangeIngredients = this.onChangeIngredients.bind(this);
+		this.onChangePrice = this.onChangePrice.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+
 		this.state = {
-			id: '',
 			name: '',
-            ingredients: '',
+            ingredients: [],
             price: 0
         }
 	}
-
-	changeHandler = e => {
-		this.setState({ [e.target.name]: e.target.value })
-	}
 	
-	submitHandler = e => {
+	onChangeName(e) {
+		this.setState({
+			name: e.target.value
+		});
+	}
+
+	onChangeIngredients(e) {
+		this.setState({
+			ingredients: e.target.value
+		});
+	}
+
+	onChangePrice(e) {
+		this.setState({
+			price: e.target.value
+		});
+	}
+
+	onSubmit(e) {
 		e.preventDefault();
-		console.log(this.state);
+
+		const ingredientsArray = this.state.ingredients.split(', ');
+		
+		const fastfood = {
+			name: this.state.name,
+			ingredients: ingredientsArray,
+			price: this.state.price
+		}
+
 		axios
-			.post(`${this.baseUrl}/fastfoods`, this.state)
-			.then(response => {
-				console.log(response);
-				alert('Fastfood cadastrado com sucesso!\n' + JSON.stringify(response.data));
-			})
-			.catch(error => {
-				alert('Ocorreu um erro, consulte os logs!');
-				console.log(error);
-			})
+		.post(`${this.baseUrl}/fastfoods`, fastfood)
+		.then(response => {
+			alert('Fastfood cadastrado com sucesso!\n' + JSON.stringify(response.data));
+			window.location = '/';
+		})
+		.catch(error => {
+			alert('Ocorreu um erro, tente novamente, consulte os logs!');
+			console.log(error);
+		})
+
 	}
 
 	render() {
-		const { name, ingredients, price } = this.state;
 		return (
-			<form onSubmit={this.submitHandler}>
-				<label>
-				Nome
-				<br />
-				<input
-					name="name"
-					type="text"
-					value={name}
-					onChange={this.changeHandler}
-				/>
-				</label>
-				<br /><br />
-				<label>
-				Ingredientes
-				<br />
-				<input
-					name="ingredients"
-					type="text"
-					value={ingredients}
-					onChange={this.changeHandler}
-				/>
-				</label>
-				<br /><br />	
-				<label>
-				Valor
-				<br />
-				<input
-					name="price"
-					type="number"
-					value={price}
-					step="any"
-					onChange={this.changeHandler}
-				/>
-				</label>
-				<br /><br />
-				<button type="submit">Salvar</button>
-				<br /><br />
-				{/* <Link to="/list">Lista de fastfoods</Link> */}
-			</form>
+			<div>
+				<h4>Cadastro de Fastfood</h4>
+				<form onSubmit={this.onSubmit}>
+					<div className="form-group">
+						<label>Nome</label>
+						<input
+							type="text"
+							className="form-control"
+							value={this.state.name}
+							onChange={this.onChangeName}
+						/>
+					</div>
+					<div className="form-group">
+						<label>Ingredientes</label>
+						<input
+							type="text"
+							className="form-control"
+							value={this.state.onChangeIngredients}
+							onChange={this.onChangeIngredients}
+						/>
+					</div>
+					<div className="form-group">
+						<label>Valor</label>
+						<input
+							type="text"
+							className="form-control"
+							value={this.state.price}
+							onChange={this.onChangePrice}
+						/>
+					</div>
+					<div className="form-group">
+						<input type="submit" value="Salvar" className="btn btn-primary"/>
+					</div>
+				</form>
+			</div>
 		)
 	}
 }
-
-export default FastfoodForm
